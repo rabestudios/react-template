bcd747f72f19341d8045e6120ad638dde5cc8317
Chris Rabe <chris.rabe14@gmail.com>
2020-08-21 01:24:44 +1000

    added cards on home page

--- src/components/routes/Home/index.tsx
+++ src/components/routes/Home/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { Grid, Paper } from '@material-ui/core'
+import styled from 'styled-components'
+
+const MainContainer = styled(Grid)`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  padding: 20px;
+`
 
 const Home: React.FC = () => {
-  return <div>Home page</div>
+  return (
+    <MainContainer container>
+      <Grid item xs={3}>
+        <Paper style={{padding: 50}} />
+      </Grid>
+      <Grid item xs={3}>
+        <Paper style={{padding: 50}} />
+      </Grid>
+      <Grid item xs={3}>
+        <Paper style={{padding: 50}} />
+      </Grid>
+      <Grid item xs={3}>
+        <Paper style={{padding: 50}} />
+      </Grid>
+    </MainContainer>
+  )
 }
 
 export default Home
